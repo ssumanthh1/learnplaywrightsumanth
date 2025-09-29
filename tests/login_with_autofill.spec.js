733d01ea70e59a083c8c14adcd28fb12ddd65aef
ssumanthh1 <ssumanthh1@gmail.com>
2025-09-29 07:25:00 +0530

    Update login_with_autofill.spec.js

--- tests/login_with_autofill.spec.js
+++ tests/login_with_autofill.spec.js
@@ -4,10 +4,9 @@ test('login with autofill method', async ({ page }) => {
 
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     const value1=(await page.locator("//p[text()='Username : Admin']").textContent()).substring(11)
-    const value2=await page.locator("//p[contains(.,'admin123')]").textContent().substring(11)
+    const value2=(await page.locator("//p[contains(.,'admin123')]").textContent()).substring(11)
     // console.log(value1)
-    // console.log(value1.substring(11))
-    // console.log(value2.substring(11))
+    // console.log(value2)
     
     await page.locator("input[name='username']").fill(value1)
     await page.locator("input[name='password']").fill(value2)
