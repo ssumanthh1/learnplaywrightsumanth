a0a231267761bc108a56f3184d976a432e701d59
ssumanthh1 <ssumanthh1@gmail.com>
2025-09-29 07:51:34 +0530

    assesment 04

---
+++ tests/OHRM_login_withautofill.spec.js
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+test('login_with_autofill method', async ({ page }) => {
+
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    const value1=(await page.locator("//p[text()='Username : Admin']").textContent()).substring(11)
+    const value2=(await page.locator("//p[contains(.,'admin123')]").textContent()).substring(11)
+    
+    // console.log(value1)
+    // console.log(value2)
+    
+    await page.locator("input[name='username']").fill(value1)
+    await page.locator("input[name='password']").fill(value2)
+    await page.keyboard.press('Enter')
+    await expect(page.locator("//h6[text()='Dashboard']")).toHaveText('Dashboard')
+
+    
+
+
+
+})
